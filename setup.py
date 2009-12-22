0c236d25beb9c1c3ef5f74f52f7936080b6931ab
Karol Krizka <kkrizka@sein.(none)>
2009-12-21 19:14:59 -0800

    Added .service file describing interface

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 
 setup (name='autorotate',
-       version='0.3.1',
+       version='0.4',
        description='Automatically rotates the screen of a tablet laptop based on orientation.',
        author='Karol Krizka',
        author_email='kkrizka@gmail.com',
@@ -12,5 +12,7 @@ setup (name='autorotate',
        license='GPL',
        py_modules=['libautorotate'],
        scripts=['auto-rotate.py','manual-rotate.py'],
-       data_files = [('share/doc/autorotate',['LICENSE','README']),('/etc/dbus-1/system.d',['net.krizka.autorotate.conf'])]
+       data_files = [('share/doc/autorotate',['LICENSE','README']),
+                     ('share/dbus-1/system-services',['net.krizka.autorotate.service']),
+                     ('/etc/dbus-1/system.d',['net.krizka.autorotate.conf'])]
 )
